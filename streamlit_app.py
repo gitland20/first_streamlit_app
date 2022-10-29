65f9c13b1dffbb2df8b4627c72b88b33ecca50ff
gitland20 <85372912+gitland20@users.noreply.github.com>
2022-10-29 06:08:26 +0530

    Update streamlit_app.py
    cSX

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load List contains:")
 streamlit.text(my_data_row)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding jackfruit', add_my_fruit)
