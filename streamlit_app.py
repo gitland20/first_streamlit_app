f9295761e86549ec7ee09e0aa6786b3b0a6dd533
gitland20 <85372912+gitland20@users.noreply.github.com>
2022-10-29 06:09:16 +0530

    Update streamlit_app.py
    zdfzds

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.text("The fruit load List contains:")
 streamlit.text(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding jackfruit', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
