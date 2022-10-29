3a6f4c4c686d20d4ddd2ef105cd9de4e63863c72
gitland20 <85372912+gitland20@users.noreply.github.com>
2022-10-29 07:02:53 +0530

    Update streamlit_app.py
    lk

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if streamlit.button('Get Fruit Load List'):
 def insert_row_snowflake(new_fruit):
   with my_cux.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-    return "Thanks for adding '+ new_fruit
+    return "Thanks for adding " + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
