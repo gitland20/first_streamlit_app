ff3237868c7ae5164d29acb4fea263c551c6dfb5
gitland20 <85372912+gitland20@users.noreply.github.com>
2022-10-29 07:18:11 +0530

    Update streamlit_app.py
    adDC

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_row)
 #allow
 def insert_row_snowflake(new_fruit):
-  with my_cux.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding " + new_fruit
     
